57bcb49f5a4127975a8c8b73493cf93a7c5383d0
haoK05 <magicmath2k5@gmial.com>
2025-09-15 16:20:48 +0700

    complete featue login by google & refreshToken

--- Backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ Backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -9,5 +9,9 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     boolean existsByEmail(String email);
+
+
+    Optional<User> findByEmail(String  email);
+
     Optional<User> findByUsername(String username);
 }
