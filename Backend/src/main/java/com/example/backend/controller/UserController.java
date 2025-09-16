186e57eb7c380d04fe21ecd9e84659c6d17545ff
nguyenminhquan5205@gmail.com <nguyenminhquan5205@gmail.com>
2025-09-16 13:37:08 +0700

    CR for member by admin

--- Backend/src/main/java/com/example/backend/controller/UserController.java
+++ Backend/src/main/java/com/example/backend/controller/UserController.java
@@ -1,27 +1,46 @@
 package com.example.backend.controller;
 
+import com.example.backend.dto.request.CreationUserRequest;
 import com.example.backend.dto.request.RegisterRequest;
 import com.example.backend.dto.response.ApiResponse;
+import com.example.backend.dto.response.CreationUserResponse;
 import com.example.backend.dto.response.RegisterResponse;
+import com.example.backend.entity.User;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.example.backend.service.UserService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users")
+@RequestMapping("/admin")
 @Slf4j
 public class UserController {
     private final UserService userService;
-    @PostMapping("/register")
-    public ApiResponse<RegisterResponse> register(@RequestBody @Valid  RegisterRequest request){
+//    @PostMapping("/register")
+//    public ApiResponse<RegisterResponse> register(@RequestBody @Valid  RegisterRequest request){
+//
+//        RegisterResponse registerResponse = userService.createUser(request);
+//        return ApiResponse.<RegisterResponse>builder().data(registerResponse).build();
+//    }
 
-        RegisterResponse registerResponse = userService.createUser(request);
-        return ApiResponse.<RegisterResponse>builder().data(registerResponse).build();
+
+    @GetMapping("/users")
+    public ApiResponse<List<User>> getUsers(){
+        List<User> users = userService.getUsers();
+        return ApiResponse.<List<User>>builder()
+                .data(users)
+                .build();
     }
+
+    @PostMapping("/createUser")
+    public ApiResponse<CreationUserResponse> register(@RequestBody @Valid CreationUserRequest request){
+
+        CreationUserResponse creationUserResponse = userService.createUser(request);
+        return ApiResponse.<CreationUserResponse>builder().data(creationUserResponse).build();
+    }
+
 }
--- Backend/src/main/java/com/example/backend/dto/response/ApiResponse.java
+++ Backend/src/main/java/com/example/backend/dto/response/ApiResponse.java
@@ -15,4 +15,5 @@ public class ApiResponse<T> {
     private int code =1000;
     private String message;
     private T data;
+
 }
