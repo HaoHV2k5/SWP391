7669668c319ff66053492bef55012be76f45bd58
HaoHV2k5 <magicmath2k5@gmail.com>
2025-09-17 07:10:32 +0700

    match role for user

--- Backend/src/main/java/com/example/backend/mapper/UserMapper.java
+++ Backend/src/main/java/com/example/backend/mapper/UserMapper.java
@@ -4,9 +4,13 @@ import com.example.backend.dto.request.CreationUserRequest;
 import com.example.backend.dto.request.RegisterRequest;
 import com.example.backend.dto.response.CreationUserResponse;
 import com.example.backend.dto.response.RegisterResponse;
+import com.example.backend.entity.Role;
 import com.example.backend.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
@@ -19,5 +23,12 @@ public interface UserMapper {
     @Mapping(source = "email", target = "email")
     User toUser(RegisterRequest request);
     RegisterResponse toRegisterResponse(User user);
+
+//    default Set<String> mapRoles(Set<Role> roles) {
+//        if (roles == null) return Set.of();
+//        return roles.stream()
+//                .map(Role::getName) // lấy field name trong Role
+//                .collect(Collectors.toSet());
+//    }
 
 }
