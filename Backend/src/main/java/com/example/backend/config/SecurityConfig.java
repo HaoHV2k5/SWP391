62b5b1b27cbfca2ce06a6f18629b2a91c79b9298
haoK05 <magicmath2k5@gmial.com>
2025-09-25 14:50:31 +0700

    fix logic login

--- Backend/src/main/java/com/example/backend/config/SecurityConfig.java
+++ Backend/src/main/java/com/example/backend/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig  {
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(  jwtDecoder() ).jwtAuthenticationConverter(jwtAuthenticationConverter())))
                 .oauth2Login(oauth2 ->
-                        oauth2.defaultSuccessUrl("/oauth2/success",true)
+                        oauth2      //.defaultSuccessUrl("/oauth2/success",true)
                                 .failureUrl("/login?error")
                                 .redirectionEndpoint(redir -> redir.baseUri("/login/oauth2/code/google"))
                                 .successHandler((HttpServletRequest request, HttpServletResponse response, Authentication authentication) -> {
