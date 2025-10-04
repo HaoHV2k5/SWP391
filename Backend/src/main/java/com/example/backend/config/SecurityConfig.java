e464f305777d9a80dde2beeaaf2f8584bf1a3a6d
haoK05 <magicmath2k5@gmial.com>
2025-10-04 22:29:06 +0700

    fix upload img to cloudinary

--- Backend/src/main/java/com/example/backend/config/SecurityConfig.java
+++ Backend/src/main/java/com/example/backend/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig  {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {//cogig chặn quyền truy cập ai mới có được quyền dùng
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request.requestMatchers(WHITE_LIST).permitAll()
-                        .requestMatchers("/api/payment/payment-return", "/payment-ipn").permitAll()
+                        .requestMatchers("/api/payment/payment-return", "/api/payment/payment-ipn").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()   
                 )
