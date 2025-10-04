3dfee8cc92cdcf13c084597c96fadb1a483f0e99
haoK05 <magicmath2k5@gmial.com>
2025-10-04 23:23:10 +0700

    split product to vehicle & battery

--- Backend/src/main/java/com/example/backend/mapper/ProductMapper.java
+++ Backend/src/main/java/com/example/backend/mapper/ProductMapper.java
@@ -9,12 +9,17 @@ import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {VehicleMapper.class, BatteryMapper.class})
 public interface ProductMapper {
+    @Mapping(target = "vehicle", source = "vehicle")
+    @Mapping(target = "battery", source = "battery")
     Product toProduct(CreateProductRequest createProductRequest);
+    
     @Mapping(target = "sellerId", source = "seller.id")
     @Mapping(target = "sellerName", source = "seller.fullname")
     @Mapping(target = "reason", source = "reason")
+    @Mapping(target = "vehicle", source = "vehicle")
+    @Mapping(target = "battery", source = "battery")
     ProductResponse toProductResponse(Product product);
 
     List<ProductResponse> toResponseList(List<Product> products);
