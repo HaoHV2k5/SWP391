d021e534d0050610674d1113eec5682b2eba5f57
HaoHV2k5 <magicmath2k5@gmail.com>
2025-10-07 00:30:16 +0700

    search product by parameter

--- Backend/src/main/java/com/example/backend/repository/VehicleRepository.java
+++ Backend/src/main/java/com/example/backend/repository/VehicleRepository.java
@@ -1,7 +1,10 @@
 package com.example.backend.repository;
 
+import com.example.backend.entity.Product;
 import com.example.backend.entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +15,17 @@ public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
     List<Vehicle> findByBrandAndModel(String brand, String model);
 
     List<Vehicle> findByBrandAndModelAndYearManufactured(String brand, String model, Integer yearManufactured);
+    @Query(value = """
+    select v.*,
+    match(v.model, v.brand)
+    against(:keyword in natural language mode) as score
+     from vehicles v
+   where match(v.model, v.brand)
+    against(:keyword in natural language mode)
+    order by score desc
+
+""",nativeQuery=true)
+    List<Vehicle> searchFullText(@Param("keyword") String keyword);
+
 
 }
--- Backend/src/main/java/com/example/backend/service/SearchService.java
+++ Backend/src/main/java/com/example/backend/service/SearchService.java
@@ -3,16 +3,21 @@ package com.example.backend.service;
 import com.example.backend.dto.response.ProductResponse;
 import com.example.backend.entity.Product;
 import com.example.backend.entity.Tags;
+import com.example.backend.entity.Vehicle;
 import com.example.backend.exception.AppException;
 import com.example.backend.exception.ErrorCode;
 import com.example.backend.mapper.ProductMapper;
 import com.example.backend.repository.ProductRepository;
 import com.example.backend.repository.TagsRepository;
+import com.example.backend.repository.VehicleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +25,8 @@ public class SearchService {
     private  final TagsRepository tagsRepository;
    private final ProductRepository productRepository;
     private final ProductMapper productMapper;
+    private final VehicleRepository vehicleRepository;
+    private static  final Set<String> REMOVE_WORDS = Set.of("xe", "mua", "bán", "cũ", "mới");
 
     public List<ProductResponse>  getProductByTagSlug(String slug){
         Tags tag = tagsRepository.findBySlugs(slug).orElseThrow(() -> new AppException(ErrorCode.TAG_NOT_EXIST));
@@ -30,6 +37,68 @@ public class SearchService {
 
         return  productMapper.toResponseList(list);
 
+    }
+
+
+    public List<ProductResponse> searchVehicles(String request){
+        List<String> keyword = extractWords(request);
+//        String brand = null;
+//        String model = null;
+//        for(String word : keyword){
+//            if(isBrand(word)){
+//                brand = word;
+//            }
+//            else{
+//                model = model == null? word: model+" "+word;
+//            }
+//        }
+//
+//
+        String fullTextKeyWord = String.join(" ", keyword);
+//
+//        List<Product> list = productRepository.searchFullText(fullTextKeyWord);
+//        final String finalBrand = brand;
+//        if(brand != null){
+//            list = list.stream().filter(v ->
+//                            v.getVehicle() != null && v.getVehicle().getBrand() != null &&
+//                            v.getVehicle().getBrand().equalsIgnoreCase(finalBrand))
+//                    .toList();
+//        }
+//        final String finalModel = model;
+//        if(model != null){
+//            list = list.stream().filter(v -> v.getVehicle().getModel().toLowerCase().contains(finalModel.toLowerCase()))
+//                    .toList();
+//        }
+
+        List<Product> listProduct = productRepository.searchFullText(fullTextKeyWord);
+        List<Vehicle> listVehicle =vehicleRepository.searchFullText(fullTextKeyWord);
+        List<Product> list = new ArrayList<>(listProduct);
+
+        for (Vehicle vehicle : listVehicle) {
+            Product p = productRepository.findProductByVehicle(vehicle).orElseThrow(() -> new AppException(ErrorCode.PRODUCT_NOT_FOUND));
+            list.add(p);
+
+        }
+
+        return productMapper.toResponseList(list);
+
+
+    }
+
+
+// can xem lai code
+    public static List<String> extractWords(String request){
+
+        return Arrays.stream(request.toLowerCase().split("\\s+"))
+                .filter(word -> !REMOVE_WORDS.contains(word))
+                .collect(Collectors.toList());
+
+
+    }
+
+    public static boolean isBrand(String request){
+     Set<String> brand = Set.of("VinFast","Osakar","Yadea","Pega","Dibao");
+        return brand.contains(request.toLowerCase());
     }
 
 
