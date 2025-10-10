28f2550c55843a6733d0f4066d6f28e35b33d0fb
HaoHV2k5 <magicmath2k5@gmail.com>
2025-10-10 19:51:32 +0700

    get all product posted for guest and buyer

--- Backend/src/main/java/com/example/backend/config/SecurityConfig.java
+++ Backend/src/main/java/com/example/backend/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.backend.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -60,6 +61,7 @@ public class SecurityConfig  {
                 .authorizeHttpRequests(request -> request.requestMatchers(WHITE_LIST).permitAll()
                         .requestMatchers("/api/payment/payment-return", "/api/payment/callback").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers(HttpMethod.GET,"products").permitAll()
                         .anyRequest().authenticated()   
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(  jwtDecoder() ).jwtAuthenticationConverter(jwtAuthenticationConverter())))
